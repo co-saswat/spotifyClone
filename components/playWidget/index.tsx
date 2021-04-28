926422705d02e7c145039c8ab2884cf7d77b0282
Saswat Sachin Mishra <ssm13092013@gmail.com>
2021-04-28 16:32:49 +0530

    Added Sound Property

--- components/playWidget/index.tsx
+++ components/playWidget/index.tsx
@@ -1,18 +1,76 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { Song } from '../../types';
 import styles from './styles'
 import { AntDesign, FontAwesome } from '@expo/vector-icons'
-
+import { Sound } from "expo-av/build/Audio/Sound";
 
 const song = {
   id: '1',
+  uri: 'https://not-just-trash.s3-eu-west-1.amazonaws.com/WhatsApp+Audio+2020-09-22+at+14.20.25.mp4',
   imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-  title: 'High on You',
-  artist: 'Post Melon'
+  title: 'Music Beats',
+  artist: 'Saswat'
 }
 
 const PlayWidget = () => {
+
+
+  const [sound, setSound] = React.useState<Sound | null>(null);
+  const [isPlaying, setIsPlaying] = React.useState<boolean>(true);
+  const [isLike, setIsLike] = React.useState<boolean>(false)
+
+  const onPlayerStatusUpdate = (status) => {
+    setIsPlaying(status.isPlaying);
+  }
+
+  const playCurrentSong = async () => {
+
+    if (sound) {
+      await sound.unloadAsync();
+    }
+
+    const { sound: newSound } = await Sound.createAsync(
+      { uri: song.uri },
+      { shouldPlay: isPlaying },
+      onPlayerStatusUpdate
+    )
+
+    setSound(newSound);
+
+  }
+
+  React.useEffect(() => {
+
+    playCurrentSong();
+  }, [])
+
+
+  const onPlayPausePress = async () => {
+    if (!sound) {
+      return;
+    }
+
+    if (isPlaying) {
+      await sound.stopAsync();
+    } else {
+      await sound.playAsync();
+    }
+  }
+
+
+  const onLikePress = () => {
+    if(!sound) {
+      return
+    }
+
+    if(isLike) {
+      setIsLike(false);
+    } else {
+      setIsLike(true);
+    } 
+
+  }
 
   return (
     <View style={styles.container}>
@@ -23,8 +81,12 @@ const PlayWidget = () => {
           <Text style={styles.artist}>{song.artist} </Text>
         </View>
         <View style={styles.iconsContainer}>
-          <AntDesign name="hearto" size={30} color={"white"} />
-          <FontAwesome name="play" size={30} color={"white"} />
+          <TouchableOpacity onPress={onLikePress}>
+          <AntDesign name={isLike ? 'heart' : 'hearto'} size={30} color={"white"} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={onPlayPausePress}>
+            <FontAwesome name={isPlaying ? 'pause' : 'play'} size={30} color={"white"} />
+          </TouchableOpacity>
         </View>
       </View>
     </View>
